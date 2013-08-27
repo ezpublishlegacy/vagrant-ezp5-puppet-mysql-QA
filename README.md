5b34a3ac6aaf6a2f40c281a3ee2d6afccff3d49f
Pedro Resende <pedroresende@mail.resende.biz>
2013-08-27 17:05:07 +0100

    Start adapting https://github.com/cleverti/vagrant-ezp5-puppet-mysql to Q.A.

---
+++ README.md
@@ -0,0 +1,47 @@
+# vagrant:ezp5:puppet:mysql:QA
+
+The Q.A. machine for eZ Publish 5.x, provisioned with Puppet.
+
+## Stack & utilities:
+
+- CentOS 6.4 x64
+- Apache 2.2.15
+- MySQL 5.1.69
+- PHP 5.3.3
+- APC 3.1.9
+- Xdebug 2.2.3
+- Composer
+- Git
+- VNCServer
+- SeleniumServer
+- SVN
+- VSFTP
+- Unzip
+
+## Requirements:
+
+- Vagrant (http://vagrantup.com)
+- VirtualBox (https://www.virtualbox.org/) or VMWare (http://www.vmware.com/)
+
+## Getting started:
+
+- Clone this project to a directory 
+- Run `$ vagrant up` from the terminal
+- Wait (the first time will take a few minutes, as the base box is downloaded, and required packages are installed for the first time), get some coffee.
+- Done! `$ vagrant ssh` to SSH into your newly created machine. The MOTD contains details on the database, hostnames, etc.
+- VNCServer uses port 5900, which is redirected to localhost. In order to use it, use the address localhost:1
+
+
+## Environment Details:
+
+```
+MySQL:
+  default database: ezp
+  default db user: ezp
+  default db user password: ezp
+
+Apache/httpd:
+  www root: /var/www/html/ezpublish5
+```
+
+Built @ [cleverti](http://www.cleverti.com) by Pedro Resende
---
+++ manifests/base_xdebug.pp
@@ -0,0 +1,266 @@
+include ntpd
+include apachephp
+include db
+include createdb
+include apc
+include xdebug
+include imagick
+include ezfind
+include virtualhosts
+include firewall
+include composer
+include prepareezpublish
+include motd
+include addhosts
+include addtostartup
+include git
+include vncserver
+include seleniumserver
+
+class ntpd {
+    package { "ntpdate.x86_64": 
+      ensure => installed 
+    }
+    service { "ntpd":
+      ensure => running,
+    }
+}
+
+class motd {
+    file    {'/etc/motd':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/motd/motd.xdebug.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+    }
+}
+
+class git {
+    package { "git":
+      ensure => installed,
+    }
+}
+
+class vncserver {
+    $neededpackages = [ "tigervnc", "tigervnc-server", "tigervnc-server-module", "xterm" ]
+    package { $neededpackages:
+      ensure => present,
+    } ~>
+    file { "/home/vagrant/.Xauthority":
+      ensure => file,
+      content => template('/tmp/vagrant-puppet/manifests/vncserver/Xauthority.erb'),
+    } ~>
+    file { "/home/vagrant/.vnc/xstartup":
+      ensure => file,
+      content => template('/tmp/vagrant-puppet/manifests/vncserver/xstartup.erb'),
+    }
+}
+
+class seleniumserver {
+    exec { "create selenium folder":
+      command => "/bin/mkdir /opt/selenium",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      returns => [ 0, 1, '', ' ']
+    } ~>
+    exec    { "wget":
+      cwd     => "/opt/selenium",
+      command => "/usr/bin/wget http://selenium.googlecode.com/files/selenium-server-standalone-2.5.0.jar",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      returns => [ 0, 1, '', ' ']
+    } ~>
+    file { "/usr/local/bin/start_seleniumrc.sh":
+      ensure => file,
+      content => template('/tmp/vagrant-puppet/manifests/selenium/start_seleniumrc.sh.erb'),
+    } ~>
+    exec    { "chmod":
+      command => "/bin/chmod +x /user/local/bin/start_seleniumrc.sh",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      returns => [ 0, 1, '', ' ']
+    } 
+}
+
+class apachephp {
+    $neededpackages = [ "httpd", "php", "php-cli", "php-gd" ,"php-mysql", "php-pear", "php-xml", "php-mbstring", "php-pecl-apc", "php-process", "curl.x86_64" ]
+    package { $neededpackages:
+        ensure => present,
+    } ~>
+    file    {'/etc/httpd/conf.d/01.accept_pathinfo.conf':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/httpd/01.accept_pathinfo.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+    } ~>
+    file    {'/etc/php.d/php.ini':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/php/php.ini.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+    } 
+}
+
+class imagick {
+    $neededpackages = [ "ImageMagick", "ImageMagick-devel", "ImageMagick-perl" ]
+    package { $neededpackages:
+      ensure => installed
+    }
+    exec    { "update-channels":
+      command => "pear update-channels",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      require => Package['php-pear'],
+      returns => [ 0, 1, '', ' ']
+    } ~>
+    exec    { "install imagick":
+      command => "pecl install imagick",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      require => Package['php-pear'],
+      returns => [ 0, 1, '', ' ']
+    }
+}
+
+class db {
+    $neededpackages = [ "mysql", "mysql-server"]
+    package { $neededpackages:
+      ensure => installed
+    }
+    file    {'/etc/my.cnf':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/mysql/my.cnf.erb'),
+      owner   => 'root',
+      mode    => '644',
+    }
+    service { "mysqld":
+      ensure => running,
+      hasstatus => true,
+      hasrestart => true,
+      require => Package["mysql-server"],
+      restart => true;
+    }
+}
+
+ class apc {
+    $neededpackages = [ "php-devel", "httpd-devel", "pcre-devel.x86_64", "php-pecl-apc.x86_64" ]
+     package { $neededpackages:
+       ensure => installed
+    } ~>
+   file    {'/etc/php.d/apc.ini':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/php/apc.ini.erb'),
+    }
+ }
+
+class createdb {
+    exec { "create-ezp-db":
+      command => "/usr/bin/mysql -uroot -e \"create database ezp character set utf8; grant all on ezp.* to ezp@localhost identified by 'ezp';\"",
+      require => Service["mysqld"],
+      returns => [ 0, 1, '', ' ']
+    }
+}
+
+class xdebug {
+    exec    { "install xdebug":
+      command => "pear install pecl/xdebug",
+      path    => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin",
+      require => Package['php-pear'],
+      returns => [ 0, 1, '', ' ']
+    }
+    file    {'/etc/php.d/xdebug.ini':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/php/xdebug.ini.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+      require => Package["php"],
+    }
+}
+
+class ezfind {
+    package { "java-1.6.0-openjdk.x86_64":
+      ensure => installed
+    }
+}
+
+class virtualhosts {
+    file    {'/etc/httpd/conf.d/02.namevirtualhost.conf':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/virtualhost/02.namevirtualhost.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+      require => Package["httpd"],
+    }
+    file    {'/etc/httpd/conf.d/ezp5.conf':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/virtualhost/ezp5.xdebug.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+      require => Package["httpd"],
+    }
+}
+
+class firewall {
+    file    {'/etc/sysconfig/iptables':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/iptables/iptables.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '600',
+    }
+    service { iptables:
+      ensure => running,
+      subscribe => File["/etc/sysconfig/iptables"],
+    }
+}
+
+class composer {
+    exec    { "get composer":
+      command => "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin",
+      path    => "/usr/local/bin:/usr/bin/",
+      require => Package["httpd"],
+      returns => [ 0, 1, '', ' ']
+    } ~>
+    exec    { "link composer":
+      command => "/bin/ln -s /usr/local/bin/composer.phar /usr/local/bin/composer.phar",
+      path    => "/usr/local/bin:/usr/bin/:/bin",
+      returns => [ 0, 1, '', ' ']
+    }
+}
+
+class prepareezpublish {
+    service { 'httpd':
+      ensure => running,
+      enable => true,
+      require => [File['/etc/httpd/conf.d/01.accept_pathinfo.conf'], File['/etc/httpd/conf.d/ezp5.conf']]
+    }
+}
+
+class addhosts {
+    file    {'/etc/hosts':
+      ensure  => file,
+      content => template('/tmp/vagrant-puppet/manifests/hosts/hosts.xdebug.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+    }
+}
+
+class addtostartup {
+    exec    { "add httpd to startup":
+      command => "/sbin/chkconfig httpd on",
+      path    => "/usr/local/bin/:/bin/",
+      require => Package["httpd", "php", "php-cli", "php-gd" ,"php-mysql", "php-pear", "php-xml", "php-mbstring", "php-pecl-apc", "php-process", "curl.x86_64"]
+    } ~>
+    exec    { "add mysql to startup":
+      command => "/sbin/chkconfig --add mysqld",
+      path    => "/usr/local/bin/:/bin/",
+      require => Package["httpd", "php", "php-cli", "php-gd" ,"php-mysql", "php-pear", "php-xml", "php-mbstring", "php-pecl-apc", "php-process", "curl.x86_64"]
+    } ~>
+    exec    { "add mysql":
+      command => "/sbin/chkconfig mysqld on",
+      path    => "/usr/local/bin/:/bin/",
+      require => Package["httpd", "php", "php-cli", "php-gd" ,"php-mysql", "php-pear", "php-xml", "php-mbstring", "php-pecl-apc", "php-process", "curl.x86_64"]
+    }
+}
