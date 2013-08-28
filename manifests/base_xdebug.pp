14f72570bb8312ab08d536484f013ac4aa97d3f0
Pedro Resende <pedroresende@mail.resende.biz>
2013-08-28 10:27:51 +0100

    Fix /var/www/html folder permissions

--- manifests/base_xdebug.pp
+++ manifests/base_xdebug.pp
@@ -265,6 +265,12 @@ class prepareezpublish {
       ensure => running,
       enable => true,
       require => [File['/etc/httpd/conf.d/01.accept_pathinfo.conf'], File['/etc/httpd/conf.d/ezp5.conf']]
+    } ~>    
+    file { "/var/www/html":
+      ensure => "directory",
+      owner  => "vagrant",
+      group  => "vagrant",
+      mode   => 777,  
     }
 }
 
