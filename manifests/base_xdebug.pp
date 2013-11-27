20f79a047abd940145a9ed956d3dce9d72e5ee3e
Pedro Resende <pedroresende@mail.resende.biz>
2013-11-27 16:04:31 +0000

    Fix QA-160 - As QA i want to generate ssh-keys on all vagrant machines on the fly

--- manifests/base_xdebug.pp
+++ manifests/base_xdebug.pp
@@ -1,4 +1,5 @@
 include ntpd
+include generatessh
 include bashrc
 include ssh
 include apachephp
@@ -29,6 +30,13 @@ class ntpd {
     }
     service { "ntpd":
       ensure => running,
+    }
+}
+
+class generatessh {
+    exec { "ssh-keygen":
+        command => "ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''",
+        path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
     }
 }
 
