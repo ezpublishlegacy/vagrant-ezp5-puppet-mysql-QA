62657d12fb02345c65c8f420ab453156cbcfc3a6
Pedro Resende <pedroresende@mail.resende.biz>
2014-10-24 09:00:19 +0100

    Fixed missing boot mariaDB on startup

--- modules/mariadb/manifests/init.pp
+++ modules/mariadb/manifests/init.pp
@@ -1,14 +1,18 @@
 class mariadb {
     $neededpackages = [ "mariadb", "mariadb-server"]
     package { $neededpackages:
-      ensure => installed,
-      require => Class["system"]
+      ensure      => installed,
+      require     => Class["system"]
     } ~>
     service { "mariadb":
-      ensure => running,
-      hasstatus => true,
-      hasrestart => true,
-      require => Package["mariadb-server"],
-      restart => true;
+      ensure      => running,
+      hasstatus   => true,
+      hasrestart  => true,
+      require     => Package["mariadb-server"],
+      restart     => true;
+    } ~>
+    exec { 'Add Mariadb to Boot':
+      command     => '/usr/bin/systemctl enable mariadb.service',
+      path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     }
 }
